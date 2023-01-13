8c29643619b5f067880804d6657e4f84d3ec8254
Ace <aceonsteam001@gmail.com>
2023-01-13 19:46:38 +0800

    add the content of front-end

---
+++ js/template.js
@@ -0,0 +1,236 @@
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn the basics - HTML';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Emmet</li>
+<li>Forms</li>
+<li>Semantic HTML</li>
+<li>SEO Basics</li>`;
+container.appendChild(clone);
+
+var clone = template.content.cloneNode(true);
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn basic usage of Version Control System';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Git</li>
+<li>GitHub</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn to deploy your websites';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Netlify</li>
+<li>Vercel</li>
+<li>Firebase</li>
+<li>Github Pages</li>
+<li>Heroku</li>
+<li>Render</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn the basics - JS';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Basic Syntax</li>
+<li>DOM Manipulation</li>
+<li>Fetch API / Ajax</li>
+<li>Async Await</li>
+<li>Event Listeners</li>
+<li>ES6+ JavaScript</li>
+<li>Promises</li>
+<li>Classes</li>
+<li>Array Methods</li>
+<li>Scoping</li>
+<li>Hoisting</li>
+<li>Closures</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn any CSS Framework';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Bootstrap</li>
+<li>Tailwind</li>
+<li>Materialize</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn any CSS Preprocessor mostly SASS and SCSS';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>SASS / SCSS</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn the basics of Package Managers';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>NPM</li>
+<li>Yarn</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Pick a JavaScript Framework / Library';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>React</li>
+<li>Vue</li>
+<li>Angular</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Basic things to learn in React';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Components</li>
+<li>JSX</li>
+<li>Props</li>
+<li>State</li>
+<li>Events</li>
+<li>JSX</li>
+<li>Conditional Rendering</li>`;
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn some of the React UI Frameworks';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Material UI</li>
+<li>Ant Design</li>
+<li>Chakra UI</li>
+<li>React Bootstrap</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn to use popular React packages';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>React Router</li>
+<li>React Query</li>
+<li>Axios</li>
+<li>React Hook Form</li>
+<li>Styled Components</li>
+<li>Storybook</li>
+<li>Framer Motion</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn how to manage state in React with state management tools';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Redux</li>
+<li>MobX</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Things to learn after learning React';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Next JS</li>
+<li>Gatsby</li>
+<li>TypeScript</li>
+<li>React Native</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Important things to learn in Next JS';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Static Site Generation</li>
+<li>Server Side Rendering</li>
+<li>Incremental Static Regeneration</li>
+<li>Dynamic Pages</li>
+<li>CSS / SASS Modules</li>
+<li>Lazy loading Modules</li>
+<li>API Routes</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Learn to test your apps with some of these libraries / frameworks ';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Jest</li>
+<li>Testing Library</li>
+<li>Cypress</li>
+<li>Dynamic Pages</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Other important topics you should know';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>PWA</li>
+<li>Web Sockets</li>
+<li>CORS</li>
+<li>JSON</li>
+<li>RESTful APIs</li>
+<li>GraphQL APIs</li>
+<li>Basic Security</li>
+<li>Web Accessibility</li>`
+container.appendChild(clone);
+
+var template = document.getElementById("myNavTemplate");
+var container = document.querySelector('.f-roadmap');
+var clone = template.content.cloneNode(true);
+var heading = clone.querySelector('h2');
+heading.textContent = 'Optional Thing to learn';
+var list = clone.querySelector('ul');
+list.innerHTML = `
+<li>Webpack</li>
+<li>Parcel</li>
+<li>SnowPack</li>
+<li>Rollup.js</li>
+<li>Gulp</li>`
+container.appendChild(clone);
