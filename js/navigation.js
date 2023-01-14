ea662b98bdf34a6a0cba2dd758b1dbb0679e4bad
Ace <aceonsteam001@gmail.com>
2023-01-14 22:13:28 +0800

    add the first row of useful sites section

--- js/navigation.js
+++ js/navigation.js
@@ -7,3 +7,28 @@ hamburger.addEventListener("click", function () {
 
   mobile_menu.classList.toggle('is-open')
 });
+
+// useful website js
+const googleFonts = document.getElementById('google-f');
+const fontAwesome = document.getElementById('f-awesome');
+const codingFantasy = document.getElementById('c-fantasy');
+
+
+googleFonts.addEventListener('click', function() {
+  googleFonts.classList.toggle('expanded');
+  fontAwesome.classList.remove('expanded');
+  codingFantasy.classList.remove('expanded');
+});
+
+
+fontAwesome.addEventListener('click', function() {
+  fontAwesome.classList.toggle('expanded');
+  googleFonts.classList.remove('expanded');
+  codingFantasy.classList.remove('expanded');
+});
+
+codingFantasy.addEventListener('click', function() {
+  codingFantasy.classList.toggle('expanded');
+  googleFonts.classList.remove('expanded');
+  fontAwesome.classList.remove('expanded');
+});
